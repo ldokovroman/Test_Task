98e8429ab2a4704221c2c37951fa459f860cac5c
Ldokov Roman <71010449+ldokovroman@users.noreply.github.com>
2023-07-03 17:02:37 +0300

    Add files via upload

--- Test_Task/video_generator/urls.py
+++ Test_Task/video_generator/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from video_generator.views import create_video
+from video_generator.views import scrolling_text
 
 urlpatterns = [
     path("", scrolling_text, name="scrolling_text")
